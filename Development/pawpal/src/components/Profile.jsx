78a588d938778e65bb04c3740572f9cb8ba9509a
artemch5a1 <artemch5a1@yandex.ru>
2025-05-30 19:14:58 +0300

    styles correct

--- Development/pawpal/src/components/Profile.jsx
+++ Development/pawpal/src/components/Profile.jsx
@@ -3,8 +3,8 @@
 export const Profile = ({profile, goToMain, profilePhoto}) => {
 return (
     <div className="profile-page">
-      <div className="formsign">
-        <h2 className="titleform">Личный кабинет</h2>
+      <div className="formprofile">
+        <h2 className="titleformforProfile">Личный кабинет</h2>
 
         <img
           src={profilePhoto}
@@ -12,14 +12,14 @@ return (
           className="profile-photo"
         />
 
-        <div className="input-group">
-          <input className="input-field" type="text" value={profile.lastName} readOnly />
-          <input className="input-field" type="text" value={profile.name} readOnly />
-          <input className="input-field" type="email" value={profile.email} readOnly />
-          <input className="input-field" type="text" value={profile.birthDate} readOnly />
+        <div className="input-fieldforProf">
+          <input className="input-groupforProf" type="text" value={profile.lastName} readOnly />
+          <input className="input-groupforProf" type="text" value={profile.name} readOnly />
+          <input className="input-groupforProf" type="email" value={profile.email} readOnly />
+          <input className="input-groupforProf" type="text" value={profile.birthDate} readOnly />
         </div>
 
-        <button className="button-style" onClick={goToMain}>
+        <button className="button-styleforProfile" onClick={goToMain}>
           Выйти
         </button>
       </div>
